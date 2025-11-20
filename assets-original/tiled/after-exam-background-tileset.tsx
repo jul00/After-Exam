c5369fdd7de5a33a615c32770147892d2425ccc9
jul00 <jbretana08@gmail.com>
2025-11-20 22:42:36 +0800

    pushpush

--- assets-original/tiled/after-exam-background-tileset.tsx
+++ assets-original/tiled/after-exam-background-tileset.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="after-exam-background-tileset" tilewidth="8" tileheight="8" tilecount="360" columns="20">
  <image source="../gimp/bg-assets.png" width="160" height="144"/>
+ <wangsets>
+  <wangset name="Unnamed Set" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
 </tileset>

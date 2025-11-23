ea1bfa76ecbd8b68e24af8013c1089e60e2a546d
jul00 <jbretana08@gmail.com>
2025-11-24 00:30:53 +0800

    made bossfight, no attacks from boss yet

---
+++ assets-original/tiled/binary-tileset.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="binary-tileset" tilewidth="8" tileheight="8" tilecount="8" columns="4">
+ <editorsettings>
+  <export target="binary-tileset.tsx" format="tsx"/>
+ </editorsettings>
+ <image source="../gimp/binary.png" width="32" height="16"/>
+</tileset>
